edc89834f160cf5a302e4785eb48cec45ba5c460
rosenjens <rosen.jens@gmail.com>
2016-09-28 23:04:00 +0200

    Vig encode

--- java/Vigenere.java
+++ java/Vigenere.java
@@ -6,13 +6,23 @@ class Vigenere {
         this.alphabet = alphabet;
         this.key = key;
     }
+
     public String encode(String str){
-    
-        return ""; 
+        String code = "";
+        for (int i = 0; i < str.length(); i++){
+            int c = alphabet.indexOf(str.charAt(i));
+            if (c != -1)
+                code += alphabet.charAt( (c + alphabet.indexOf(key.charAt(i%key.length()))) % alphabet.length());
+            else
+                code += str.charAt(i);
+        }
+        return code; 
     }
+
     public String decode(String str){
     
         return "";
-    } 
+    }
+
 }
 
