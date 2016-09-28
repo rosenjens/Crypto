c45cd962f33e39e2caa4b1224548d52f25f3d460
rosenjens <rosen.jens@gmail.com>
2016-09-28 18:23:17 +0200

    fundation

---
+++ java/Vigenere.java
@@ -0,0 +1,18 @@
+class Vigenere {
+    private String alphabet;
+    private String key;
+
+    public Vigenere(String key, String alphabet){
+        this.alphabet = alphabet;
+        this.key = key;
+    }
+    public String encode(String str){
+    
+        return ""; 
+    }
+    public String decode(String str){
+    
+        return "";
+    } 
+}
+
