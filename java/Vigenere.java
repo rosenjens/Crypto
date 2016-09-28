d3244eab8f62a829c265a1f04fb7c73a90ecfba6
rosenjens <rosen.jens@gmail.com>
2016-09-29 00:22:08 +0200

    setter, getter for alphabet and key

--- java/Vigenere.java
+++ java/Vigenere.java
@@ -3,34 +3,59 @@ class Vigenere {
     private String key;
 
     public Vigenere(String key, String alphabet){
+        if (key == null || alphabet == null){
+            throw new IllegalArgumentException();
+        }
         this.alphabet = alphabet;
         this.key = key;
     }
 
-    public String encode(String str){
-        String code = "";
-        for (int i = 0; i < str.length(); i++){
-            int c = alphabet.indexOf(str.charAt(i));
-            if (c != -1)
-                code += alphabet.charAt((c + alphabet.indexOf(key.charAt(i%key.length()))) % alphabet.length());
-            else
-                code += str.charAt(i);
-        }
+    public void changeAlphabet(String alphabet){
+        this.alphabet = alphabet;
+    }
+    
+    public void changeKey(String key){
+        this.key = key;
+    }
+    
+    public String getAlphabet(){
+        return alphabet;
+    }
+    
+    public String getKey(){
+        return key;
+    }
 
-        return code; 
+    public String encode(String str){
+        return vig(str, '+');
     }
 
     public String decode(String code){
+        return vig(code, '-');
+    }
+
+    private String vig(String s, char sign){
         String str = "";
-        for (int i = 0; i < code.length(); i++){
-            int c = alphabet.indexOf(code.charAt(i));
-            if (c != -1)
-                str += alphabet.charAt((c - alphabet.indexOf(key.charAt(i%key.length()))) % alphabet.length());
-            else
-                str += code.charAt(i);
+
+        if (key == "" || alphabet == ""){
+            str = s;
+        }else{
+            for (int i = 0; i < s.length(); i++){
+                int c = alphabet.indexOf(s.charAt(i));
+                if (c != -1){
+                    if (sign == '+'){
+                        str += alphabet.charAt((c + alphabet.indexOf(key.charAt(i%key.length()))) % alphabet.length());
+                    }else{
+                        str += alphabet.charAt((c - alphabet.indexOf(key.charAt(i%key.length())) + alphabet.length()) % alphabet.length());
+                    }
+                }else{
+                    str += s.charAt(i);
+                }
+            }
         }
 
         return str;
+
     }
 
 }
