0f2c00a8e901aab97493b88d9a4bfe8ebbbc3896
rosenjens <rosen.jens@gmail.com>
2016-10-09 23:55:16 +0200

    Added base64 functionality

--- java/Vigenere.java
+++ java/Vigenere.java
@@ -1,10 +1,13 @@
+import java.util.Base64;
+import java.io.UnsupportedEncodingException;
+
 public class Vigenere extends VigenereBase{
 
-    public Vigenere(String alphabet){
+    public Vigenere(String alphabet) {
         super(alphabet);
     }
 
-    public String encrypt(String str, String key, boolean toCapitals, boolean trim){
+    public String encrypt(String str, String key, boolean toCapitals, boolean trim) {
         if (toCapitals) {
             str = str.toUpperCase();
             key = key.toUpperCase();
@@ -16,5 +19,26 @@ public class Vigenere extends VigenereBase{
             str = newStr;
         }
         return encrypt(str, key);
+    }
+
+    public String encryptBase64(String str, String key){
+        String code = "";
+        try {
+            code = Base64.getEncoder().encodeToString(str.getBytes("utf-8"));
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encrypt(code,key) ;
+    }
+
+    public String decryptBase64(String code, String key){
+        String str = "";
+        try {
+            str = new String(Base64.getDecoder().decode(decrypt(code,key)), "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return str;
     }
 }
